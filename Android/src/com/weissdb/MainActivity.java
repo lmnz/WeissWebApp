6eb7993b2df7a9520775688748323ac2163e513b
Alex Chiou <chessnone@yahoo.com>
2014-01-09 13:37:21 -0800

    Minor bug fix concerning the erase/back buttons. Made compatible with more versions.

--- Android/src/com/weissdb/MainActivity.java
+++ Android/src/com/weissdb/MainActivity.java
@@ -107,6 +107,7 @@ public class MainActivity extends Activity
 				setContentView(R.layout.activity_main);
 				currentPage = "";
 				getActionBar().setDisplayHomeAsUpEnabled(false);
+				invalidateOptionsMenu();
 				return true;
 			}
 		}
